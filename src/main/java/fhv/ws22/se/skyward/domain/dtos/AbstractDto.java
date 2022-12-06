d4ff72821b09b38645ea25afb155d0f797aa38c0
flo2002 <florian.schiemer003@gmail.com>
2022-12-06 20:44:45 +0100

    Dtos Serializable

--- src/main/java/fhv/ws22/se/skyward/domain/dtos/AbstractDto.java
+++ src/main/java/fhv/ws22/se/skyward/domain/dtos/AbstractDto.java
@@ -3,9 +3,10 @@ package fhv.ws22.se.skyward.domain.dtos;
 import fhv.ws22.se.skyward.domain.model.AbstractModel;
 import org.modelmapper.ModelMapper;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public abstract class AbstractDto {
+public abstract class AbstractDto implements Serializable {
     protected ModelMapper modelMapper;
     private UUID id;
 
