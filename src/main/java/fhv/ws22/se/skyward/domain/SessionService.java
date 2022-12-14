d9603dc7008b68bcba648bcc7b6a84b9122d303d
flo2002 <florian.schiemer003@gmail.com>
2022-12-14 11:04:12 +0100

    added tmpDataService

--- src/main/java/fhv/ws22/se/skyward/domain/SessionService.java
+++ src/main/java/fhv/ws22/se/skyward/domain/SessionService.java
@@ -1,14 +1,11 @@
 package fhv.ws22.se.skyward.domain;
 
 import fhv.ws22.se.skyward.domain.dtos.AbstractDto;
-import fhv.ws22.se.skyward.domain.dtos.BookingDto;
-import fhv.ws22.se.skyward.domain.dtos.InvoiceDto;
 import fhv.ws22.se.skyward.domain.dtos.RoomDto;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,17 +14,5 @@ public interface SessionService extends Remote {
     <T extends AbstractDto> void add(T t) throws RemoteException;
     <T extends AbstractDto> void update(UUID id, T t) throws RemoteException;
     <T extends AbstractDto> void delete(UUID id, Class<T> clazz) throws RemoteException;
-
     List<RoomDto> getAvailableRooms(LocalDateTime checkIn, LocalDateTime checkOut) throws RemoteException;
-    List<RoomDto> filterRooms(List<RoomDto> rooms, HashMap<String, Boolean> filterMap) throws RemoteException;
-    void setRoomFilterMap(HashMap<String, Boolean> filterMap) throws RemoteException;
-    HashMap<String, Boolean> getRoomFilterMap() throws RemoteException;
-
-    BookingDto getTmpBooking() throws RemoteException;
-    void resetTmpBooking() throws RemoteException;
-    void setTmpBooking(BookingDto booking) throws RemoteException;
-
-    InvoiceDto getTmpInvoice() throws RemoteException;
-    void resetTmpInvoice() throws RemoteException;
-    void setTmpInvoice(InvoiceDto invoice) throws RemoteException;
 }
